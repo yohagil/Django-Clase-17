855db5bc7c132edc75b4c8a4fa9bb63927b2da1b
yohanny gil <yohanny.gil@gmail.com>
2025-12-14 18:38:26 -0400

    preparacion aplicacion core

--- core/models.py
+++ core/models.py
@@ -1,14 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class Curso(models.model):
- nombre = models.CharField(max_length=40)
- camada = models.integerfield()
-
- class Profesor(models.model):
+class Profesor(models.model):
   nombre = models.CharField(max_length=40) 
   apellido = models.CharField(max_length=20)
   email = models.CharField(max_length=40)
   apellido = models.CharField(max_length=30)
-  
-
