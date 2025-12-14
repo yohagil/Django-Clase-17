838614e8f95c3faceb875c0bf5c949b9cea04340
yohanny gil <yohanny.gil@gmail.com>
2025-12-14 19:04:12 -0400

    creacion tabla core_profesor

--- core/models.py
+++ core/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class Profesor(models.model):
+class Profesor(models.Model):
   nombre = models.CharField(max_length=40) 
   apellido = models.CharField(max_length=20)
   email = models.CharField(max_length=40)
-  apellido = models.CharField(max_length=30)
+  profesion = models.CharField(max_length=30)
